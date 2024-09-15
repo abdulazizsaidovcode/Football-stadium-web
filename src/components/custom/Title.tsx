d3ff6046bdecd874edc2d23016f18669a482acc8
erkinov26 <erkinov0247@gmail.com>
2024-09-15 17:01:33 +0500

    responsive css is corrected

--- src/components/custom/Title.tsx
+++ src/components/custom/Title.tsx
@@ -1,7 +1,7 @@
 const Title = ({ text, className }: { text: string; className?: string }) => {
 	return (
 		<h1
-			className={`font-bold text-[4vw] max-800:text-[6vw] transition hover:scale-110 ${className}`}>
+			className={`font-bold text-[4vw] max-800:text-[6vw] transition  hover:-translate-y-4 ${className}`}>
 			{text}
 		</h1>
 	);
